90fba2db137194b7dbd3e61ceafd58be0a6ae0a9
Manoj Kumar Sah <manoj23@navgurukul.org>
2024-09-06 17:37:23 +0530

    Add google login

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
-import React from 'react'
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-import SignupPage from './components/authentication/SignUp'
-import LoginPage from './components/authentication/Login'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import SignupPage from "./components/authentication/SignUp";
+import LoginPage from "./components/authentication/Login";
+import Home from "./components/Home/Home";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import {GoogleOAuthProvider} from '@react-oauth/google'
 const App = () => {
+
+  const GoogleAuthWrapper =()=>{
+    return (
+      <GoogleOAuthProvider clientId="773703183001-d44jdgn8e2pk5eo8s464rmj9hecvj6hm.apps.googleusercontent.com">
+        <LoginPage></LoginPage>
+      </GoogleOAuthProvider>
+    );
+  }
   return (
     <div>
       <Router>
-        <Navbar/>
-          <Routes>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/signup" element={<SignupPage/>}/>
-          </Routes>
-          <Footer/>
-        </Router>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <h1>
+                <Home />{" "}
+              </h1>
+            }
+          />
+          <Route path="/login" element={<GoogleAuthWrapper />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './AuthPage.css'; 
 import { Link } from 'react-router-dom';
+import {useGoogleLogin} from '@react-oauth/google'
 
 function LoginPage() {
   const navigate = useNavigate();
@@ -9,6 +10,20 @@ function LoginPage() {
   const handleClose = () => {
     navigate('/'); 
   };
+  const googleResponse = async(authResponse)=>{
+try{
+  console.log(authResponse);
+
+}catch(error){
+  console.error('Error while response in google auth code :', error);
+}
+  }
+
+  const GoogleLogin = useGoogleLogin({
+    onError: googleResponse,
+    onSuccess: googleResponse,
+    flow: "auth-code",
+  });
 
   return (
     <div className="login-container" id='/login'>
@@ -24,6 +39,8 @@ function LoginPage() {
           <input type="password" id="password" name="password" required />
         </div>
         <button type="submit" className='but'>Log In</button>
+        <p className='or'>OR</p>
+        <button className='but' onClick={GoogleLogin}>Login with Google</button>
         <span>Don't have a Account <Link to="/signup">SignUp?</Link></span>
       </form>
     </div>

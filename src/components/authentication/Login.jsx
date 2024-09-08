46cafd9bdf9892cb274ab9c13991d1066e31d277
ajaybedwall <ajaymeena606263@gmail.com>
2024-09-08 09:22:10 +0530

    add login signup authentication

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './AuthPage.css'; 
+// import './AuthPage.css'; 
 import { Link } from 'react-router-dom';
-import {useGoogleLogin} from '@react-oauth/google'
+import { useGoogleLogin } from '@react-oauth/google'
+import { handleError, handleSuccess } from "./Utils";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function LoginPage() {
   const navigate = useNavigate();
 
   const handleClose = () => {
-    navigate('/'); 
+    navigate("/"); 
   };
   const googleResponse = async(authResponse)=>{
 try{
@@ -25,26 +28,107 @@ try{
     flow: "auth-code",
   });
 
+   const [loginInfo, setLoginInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginInfo((prevLoginInfo) => ({
+      ...prevLoginInfo,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const {email, password } = loginInfo;
+    if (!email || !password) {
+      handleError("All fields are required!");
+      return;
+    }
+
+    try {
+      const url = "http://localhost:3000/auth/login";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginInfo),
+      });
+      const result = await response.json();
+      const { jwtToken, name, error } = result;
+      console.log(result); 
+
+     
+      if (result.success) {
+        handleSuccess(result.message);
+        localStorage.setItem('token', jwtToken);
+        localStorage.setItem('LoggedInUser', name);
+        setTimeout(() => {
+          navigate("/home");
+        }, 500);
+      } else if (result.error) {
+        const details = result.error.details[0]?.message || "An error occurred";
+        handleError(details);
+      } else {
+        handleError("Authentication failed: email or password is incorrect.");
+      }
+    } catch (err) {
+      handleError(err.message || "An unexpected error occurred!");
+    }
+  };
+
+
   return (
-    <div className="login-container" id='/login'>
-      <form className="login-form">
-        <button type="button" className="close-button" onClick={handleClose}>×</button>
+    <div className="login-container" id="/login">
+      <ToastContainer />
+
+      <form onSubmit={handleLogin} className="login-form">
+        <button type="button" className="close-button" onClick={handleClose}>
+          ×
+        </button>
         <h2>Login</h2>
         <div className="input-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" required />
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={handleChange}
+            type="email"
+            id="email"
+            name="email"
+            value={loginInfo.email}
+            autoFocus
+          />
         </div>
         <div className="input-group">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" required />
+          <input
+            onChange={handleChange}
+            type="password"
+            id="password"
+            name="password"
+            value={loginInfo.password}
+          />
         </div>
-        <button type="submit" className='but'>Log In</button>
-        <p className='or'>OR</p>
-        <button className='but' onClick={GoogleLogin}>Login with Google</button>
-        <span>Don't have a Account <Link to="/signup">SignUp?</Link></span>
+        <button type="submit" className="but">
+          Log In
+        </button>
+        <p className="or">OR</p>
+        <button className="but" onClick={GoogleLogin}>
+          Login with Google
+        </button>
+        <span>
+          Don't have an Account <Link to="/signup">SignUp?</Link>
+        </span>
       </form>
     </div>
   );
 }
 
 export default LoginPage;
+
+
+
--- src/components/authentication/SignUp.jsx
+++ src/components/authentication/SignUp.jsx
@@ -1,38 +1,119 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './AuthPage.css'; 
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./AuthPage.css";
+import { Link } from "react-router-dom";
+import { handleError, handleSuccess } from "./Utils";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function SignupPage() {
   const navigate = useNavigate();
 
   const handleClose = () => {
-    navigate('/'); // Go back to the previous page
+    navigate("/");
+  };
+
+  const [signupInfo, setSignupInfo] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSignupInfo((prevSignupInfo) => ({
+      ...prevSignupInfo,
+      [name]: value,
+    }));
+  };
+
+  const handleSignUP = async (e) => {
+    e.preventDefault();
+
+    const { name, email, password } = signupInfo;
+    if (!name || !email || !password) {
+      handleError("All fields are required!");
+      return;
+    }
+
+    try {
+      const url = "http://localhost:3000/auth/signup";
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signupInfo),
+      });
+      const result = await response.json();
+
+      console.log(result);
+
+      if (result.success) {
+        handleSuccess(result.message);
+        setTimeout(() => {
+          navigate("/home");
+        }, 500);
+      } else if (result.error) {
+        const details = result.error.details[0]?.message || "An error occurred";
+        handleError(details);
+      } else {
+        handleError("User already exists, you can login.");
+      }
+    } catch (err) {
+      handleError(err.message || "An unexpected error occurred!");
+    }
   };
 
   return (
-    <div className="signup-main">
+    <>
+      <ToastContainer />
       <div className="signup-container">
-        <form className="signup-form">
-          <button type="button" className="close-button" onClick={handleClose}>×</button>
+        <form onSubmit={handleSignUP} className="signup-form">
+          <button type="button" className="close-button" onClick={handleClose}>
+            ×
+          </button>
           <h2>Sign Up</h2>
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" name="email" required />
+            <input
+              onChange={handleChange}
+              type="email"
+              id="email"
+              name="email"
+              value={signupInfo.email}
+              autoFocus
+            />
           </div>
           <div className="input-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" required />
+            <label htmlFor="name">Username</label>
+            <input
+              onChange={handleChange}
+              type="text"
+              id="name"
+              name="name"
+              value={signupInfo.name}
+            />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" name="password" required />
+            <input
+              onChange={handleChange}
+              type="password"
+              id="password"
+              name="password"
+              value={signupInfo.password}
+            />
           </div>
-          <button type="submit" className='but'>Sign Up</button>
-          <p>Already have an account? <Link to="/login">Login</Link></p>
+          <button type="submit" className="but">
+            Sign Up
+          </button>
+          <p>
+            Already have an account? <Link to="/login">Login</Link>
+          </p>
         </form>
       </div>
-    </div>
+    </>
   );
 }
 

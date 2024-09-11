e6fc0767ba2bc4e02618fba3a2b6940f2bc354ca
Ansh Gupta <guptaansh912@gmail.com>
2024-09-11 23:10:46 +0530

    Conflict and pushing category cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,66 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import SignupPage from "./components/authentication/SignUp";
 import LoginPage from "./components/authentication/Login";
 import Home from "./components/Home/Home";
-import PageNotFound from "./components/PageNotFound/PageNotFound";
-import {GoogleOAuthProvider} from '@react-oauth/google'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import Category from "./components/category/Category";
 import HomeDes from "./components/Navbar/Homedes";
 import Cards from "./components/Cards/Card";
 import ProductDetail from "./components/ProductDetails/ProductDetail";
-const App = () => {
+import CategoryCards from "./components/Cards/Categorycards";
 
-  const GoogleAuthWrapper =()=>{
-    return (
-      <GoogleOAuthProvider clientId="773703183001-d44jdgn8e2pk5eo8s464rmj9hecvj6hm.apps.googleusercontent.com">
-        <LoginPage></LoginPage>
-      </GoogleOAuthProvider>
-    );
-  }
+const App = () => {
+  const GoogleAuthWrapper = () => (
+    <GoogleOAuthProvider clientId="773703183001-d44jdgn8e2pk5eo8s464rmj9hecvj6hm.apps.googleusercontent.com">
+      <LoginPage />
+    </GoogleOAuthProvider>
+  );
+
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <HomeDes />
+        <ConditionalContent />
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+// Component to handle conditional rendering
+const ConditionalContent = () => {
+  const location = useLocation();
+  const isProductDetail = location.pathname.startsWith('/Product/');
+
   return (
     <div>
-      <Router>
-        <Navbar />
-        <HomeDes/>
-        <Category/>
-        <Cards/>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <h1>
-                <Home />{" "}
-              </h1>
-            }
-          />
-          <Route path="/login" element={<GoogleAuthWrapper />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="*" element={<PageNotFound />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="/Product/:id" element={<ProductDetail/>}/>
-        </Routes>
-        <Footer />
-      </Router>
+      {!isProductDetail && (
+        <>
+          <Category />
+          <Cards />
+        </>
+      )}
+      <Routes>
+        <Route path="/login" element={<GoogleAuthWrapper />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Product/:id" element={<ProductDetail />} />
+        <Route path="/Product/category/:category" element={<CategoryCards />} />
+        
+      </Routes>
     </div>
   );
 };
+
+// Wrapper for login page with Google OAuth
+const GoogleAuthWrapper = () => (
+  <GoogleOAuthProvider clientId="773703183001-d44jdgn8e2pk5eo8s464rmj9hecvj6hm.apps.googleusercontent.com">
+    <LoginPage />
+  </GoogleOAuthProvider>
+);
 
 export default App;
